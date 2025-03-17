7955ad4118215eb7dd84d3ffd5ce44f958e861e8
Kartoffelbroot <72911337+Kartoffelbroot@users.noreply.github.com>
2025-03-17 19:52:09 +0100

    Add sword

---
+++ Assets/Scripts/SwordSwingScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSwingScript : MonoBehaviour
+{
+
+    public GameObject Sword;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(SwordSwing());
+        }
+    }
+
+    IEnumerator SwordSwing()
+    {
+        Sword.GetComponent<Animator>().Play("SwordSwing");
+        yield return new WaitForSeconds(0.3f);
+        Sword.GetComponent<Animator>().Play("New State");
+    }
+}
