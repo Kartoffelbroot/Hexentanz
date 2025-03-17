79b075c3a67f081cd6cfcbbad4b4549b3ed089b7
Kartoffelbroot <72911337+Kartoffelbroot@users.noreply.github.com>
2025-03-17 14:06:59 +0100

    add gun script

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Weapon : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30;
+    public float bulletPrefabLifeTime = 3f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Left mouse click
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+    }
+    private void FireWeapon()
+    {
+        //throw new NotImplementedException();
+        //Instanciate the Bullet
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+        // Shoot the Bullet
+        GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+        // Destroy the Bullet after time
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
